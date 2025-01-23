86b51f8af15a81241172c4787d270c3078da31f2
stktyagi <stktyagi2810@gmail.com>
2025-01-23 16:41:33 +0530

    CL args

--- main.c
+++ main.c
@@ -1,12 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <curl/curl.h>
 #include "db.h"
 #include "fetch_data.h"
 
-int main(void) {
-    const char *username, *password;
-  
+int main(int argc,char *argv[]) {
+    if(argc<3){
+        fprintf(stderr, "Not enough arguements\n");
+    }
+    char *username=(char *)malloc(strlen(argv[1])+1);
+    char *password=(char *)malloc(strlen(argv[2])+1);
+
+    if(!username || !password){
+        fprintf(stderr, "memory allocation failed\n");
+    }
+
+    strcpy(username,argv[1]);
+    strcpy(password, argv[2]);
+   printf("%s\n", username);
+    printf("%s\n", password);
+    
     if (!get_mongo_credentials(&username, &password)) {
         fprintf(stderr, "Failed to retrieve username and password from MongoDB.\n");
         return 1;
@@ -26,6 +40,7 @@ int main(void) {
         fprintf(stderr, "Missing username or password.\n");
         return 1;
     }
-
+    free(username);
+    free(password);
     return 0;
 }
